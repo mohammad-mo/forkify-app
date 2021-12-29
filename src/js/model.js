5b18539e6155f6d44e9924846216bbda369078de
Mohammad <mohammadmahone@gmail.com>
2021-12-29 17:28:59 +0330

    bookmark feature added.

--- src/js/model.js
+++ src/js/model.js
@@ -12,7 +12,8 @@ export const state =
         results: [],
         resultsPerPage: RESULTS_PER_PAGE,
         page: 1
-    }
+    },
+    bookmarks: []
 }
 
 export const loadRecipe = async (id) =>
@@ -32,6 +33,14 @@ export const loadRecipe = async (id) =>
           servings: recipe.servings,
           cookingTime: recipe.cooking_time,
           ingredients: recipe.ingredients
+        }
+        if (state.bookmarks.some(bookmark => bookmark.id === id))
+        {
+            state.recipe.bookmarked = true
+        }
+        else
+        {
+            state.recipe.bookmarked = false
         }
     }
     catch(err)
@@ -60,7 +69,7 @@ export const loadSearchResults = async (query) =>
                     image: rec.image_url
                 }
             })
-            // console.log(state.search.results)
+            state.search.page = 1
     }
     catch(err)
     {
@@ -89,4 +98,23 @@ export const updateServings = (newServings) =>
         })
 
     state.recipe.servings = newServings
+}
+
+export const addBookmark = (recipe) =>
+{
+    // Add bookmark
+    state.bookmarks.push(recipe)
+
+    // Mark current recipe as bookmarked
+    if (recipe.id === state.recipe.id) state.recipe.bookmarked = true
+}
+
+export const deleteBookmark = (id) =>
+{
+    // Delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id)
+    state.bookmarks.splice(index, 1)
+
+     // Mark current recipe as NOT bookmarked
+     if (id === state.recipe.id) state.recipe.bookmarked = false
 }
